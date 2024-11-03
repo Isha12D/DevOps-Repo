67d8bb62a6342c71720a2377795b7222091ae606
Isha12D <ishadoifode6@gmail.com>
2024-11-04 01:02:33 +0530

    addition of two nums

--- DevOpsPrep.cpp
+++ DevOpsPrep.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main(){
-    cout<<"DevOps ki taiyyari chal rhi hai.!!!!"<<endl;
-    cout<<"I also have to make some changes!!"<<endl;
+    // cout<<"DevOps ki taiyyari chal rhi hai.!!!!"<<endl;
+    // cout<<"I also have to make some changes!!"<<endl;
+    cout<<"ab kuch padh lete h!"<<endl;
+    int a = 80;
+    int b = 90;
+    cout<<a+b;
     return 0;
 }
